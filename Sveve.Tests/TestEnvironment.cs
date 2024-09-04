7091bb3b13bb1c75f26f4ac141a25597e92dac2b
MatsM16 <matdyr23@gmail.com>
2024-09-04 20:11:29 +0200

    Made app settings optional when building test project

--- Sveve.Tests/TestEnvironment.cs
+++ Sveve.Tests/TestEnvironment.cs
@@ -7,15 +7,21 @@ public static class TestEnvironment
 {
     public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
         .AddEnvironmentVariables()
-        .AddOptionalJsonResource("Sveve.Tests.appsettings.local.json")
+        .AddAppSettings()
         .Build();
 }
 
 file static class Extensions
 {
-    public static IConfigurationBuilder AddOptionalJsonResource(this IConfigurationBuilder builder, string resourceName) => 
-        builder.AddOptionalJsonStream(Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName));
-
-    public static IConfigurationBuilder AddOptionalJsonStream(this IConfigurationBuilder builder, Stream? stream) => 
-        stream is not null ? builder.AddJsonStream(stream) : builder;
+    /// <summary>
+    /// Adds appsettings.*.json files from Sveve.Tests to the configuration.
+    /// </summary>
+    public static IConfigurationBuilder AddAppSettings(this IConfigurationBuilder builder)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        foreach (var resourceName in assembly.GetManifestResourceNames())
+            if (resourceName.StartsWith("Sveve.Tests.appsettings.") && resourceName.EndsWith(".json"))
+                builder.AddJsonStream(assembly.GetManifestResourceStream(resourceName)!);
+        return builder;
+    }
 }
